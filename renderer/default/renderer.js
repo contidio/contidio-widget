fc2ab83b43fa59090ae25cd3c952eaa7ee16e84c
Lukas Struber <lukas.struber@contidio.com>
2017-05-10 13:57:27 +0200

    capsulated code, using jquery if available, domtastic as fallback, documentation basics

--- renderer/default/renderer.js
+++ renderer/default/renderer.js
@@ -1,81 +1,98 @@
-function Renderer($, options){
+/** Contidio Widget: default renderer
+ @param {Object} options
+ @param $ DOMtastic, see https://domtastic.js.org/
+ */
+function Renderer(options, $) {
 
-    this.options = options ? options : {};
+  this.options = options ? options : {};
 
-    this.init = function(){
-        this.draw();
-    };
+  /**
+   * used to render an item list view
+   * @param item
+   * @returns {*|jQuery|HTMLElement|string}
+   */
+  this.renderListView = function (item) {
 
-    this.renderListItem = function(item) {
+    var options = this.options;
 
-        var options = this.options;
+    $item = $("<div class='" + options.itemClass + " " + item.type + "'></div>");
+    $item.data("uuid", item.uuid);
 
-        $item = $("<div class='"+options.itemClass+" "+item.type+"'></div>");
-        $item.data("uuid",item.uuid);
+    $itemInner = $("<div class='contidio-item-inner'></div>");
 
-        $itemInner = $("<div class='contidio-item-inner'></div>");
+    $imageContainer = $("<div class='contidio-image-container'></div>");
 
-        $imageContainer = $("<div class='contidio-image-container'></div>");
+    if (item.binaryType) {
+      if (item.binaryType == "audio") {
+        $imageContainer.append("<i class='contidio-icon contidio-icon-audio'>&#9836;</i>");
+      } else if (item.binaryType == "video") {
+        $imageContainer.append("<i class='contidio-icon contidio-icon-video'>&#9655;</i>");
+      }
+    }
 
-        if(item.binaryType) {
-            if(item.binaryType == "audio"){
-                $imageContainer.append("<i class='contidio-icon contidio-icon-audio'>&#9836;</i>");
-            }else if(item.binaryType == "video"){
-                $imageContainer.append("<i class='contidio-icon contidio-icon-video'>&#9655;</i>");
-            }
+    if (item.previewImage) {
+      $imageContainer.append("<img class='contidio-item-image' src='" + item.previewImage + "'/>");
+    }
+
+    $itemInner.append($imageContainer);
+
+    $itemText = $("<div class='contidio-text-container'></div>");
+
+    $itemText.append("<div class='contidio-item-name'>" + item.name + "</div>");
+
+    $itemInner.append($itemText);
+
+    $item.append($itemInner);
+
+    return ($item);
+
+  };
+
+  /**
+   * Used to render an item detail view
+   * @param item
+   * @returns {*|jQuery|HTMLElement|string}
+   */
+  this.renderDetailView = function (item) {
+
+  };
+
+  /**
+   * Will be called on window resize (optional)
+   */
+  this.resize = function () {
+
+    var $entries = $(this.options.container + " ." + this.options.itemClass);
+
+    var itemsPerRow = this.getItemsPerRow();
+    var height = 0;
+
+    for(var j = 0; j < $entries.length; j++) {
+
+      $entries[j].style.height= "auto";
+
+      height = Math.max(height, $entries[j].offsetHeight);
+
+      if ((j + 1) % itemsPerRow == 0) {
+
+        for (var i = 0; i < itemsPerRow; i++) {
+
+          if (i + j < $entries.length) {
+            $entries[j - i].style.height = height + "px";
+          }
         }
-        
-        if(item.previewImage){
-            $imageContainer.append("<img class='contidio-item-image' src='"+item.previewImage+"'/>");
-        }
 
-        $itemInner.append($imageContainer);
+        height = 0;
+      }
+    }
+  };
 
-        $itemText = $("<div class='contidio-text-container'></div>");
-
-        $itemText.append("<div class='contidio-item-name'>"+item.name+"</div>");
-
-        $itemInner.append($itemText);
-
-        $item.append($itemInner);
-
-        return($item);
-
-    };
-
-    this.renderDetail = function(item) {
-
-    };
-
-    this.resize = function(){
-
-        var $entries = $(this.options.container+" ."+this.options.itemClass);
-
-        var itemsPerRow = this.getItemsPerRow();
-        var height = 0;
-
-        $entries.each(function(value, key){
-
-            $entries[key].style.height = "auto";
-
-            height = Math.max(height, value.offsetHeight);
-
-            if((key+1) % itemsPerRow  == 0){
-
-                for(var i = 0; i < itemsPerRow; i++) {
-
-                    if(i + key < $entries.length){
-                        $entries[key-i].style.height = height+"px";
-                    }
-                }
-
-                height = 0;
-            }
-        });
-    };
-
-    this.getItemsPerRow = function(){
-        return window.outerWidth > 897 ? 3 : window.outerWidth > 729 ? 2 : 1;
-    };
+  /**
+   * Renderer specific helper function
+   * @returns {number}
+   */
+  this.getItemsPerRow = function () {
+    return window.outerWidth > 897 ? 3 : window.outerWidth > 729 ? 2 : 1;
+  };
 
 }
--- src/main.js
+++ src/main.js
@@ -1,29 +1,41 @@
-var Promise = require('promise-polyfill');
-var $ = require('domtastic');
-require('whatwg-fetch');
-
-if (!window.Promise) {
-  window.Promise = Promise;
-}
-
-function ContidioWidget(){
+function ContidioWidget() {
 
   this.defaultOptions = {
     container: ".contidio-widget",
     itemClass: "contidio-item"
   };
 
-  this.mergeOptions = function(obj1,obj2){
-      var obj3 = {};
-      for (var attrname in obj1) { obj3[attrname] = obj1[attrname]; }
-      for (var attrname in obj2) { obj3[attrname] = obj2[attrname]; }
-      return obj3;
+  this.mergeOptions = function (obj1, obj2) {
+    var obj3 = {};
+    for (var attrname in obj1) {
+      obj3[attrname] = obj1[attrname];
+    }
+    for (var attrname in obj2) {
+      obj3[attrname] = obj2[attrname];
+    }
+    return obj3;
   };
 
-  this.options = (typeof contidioOptions !== "undefined") ? this.mergeOptions(this.defaultOptions,contidioOptions) : this.defaultOptions;
+  this.options = (typeof contidioOptions !== "undefined") ? this.mergeOptions(this.defaultOptions, contidioOptions) : this.defaultOptions;
   this.items = [];
 
-  this.init = function(){
+  this.init = function () {
+
+    var Promise = require('promise-polyfill');
+
+    var $;
+
+    if (typeof jQuery === 'undefined') {
+      $ = require('domtastic');
+    }else {
+      $ = jQuery;
+    }
+
+    require('whatwg-fetch');
+
+    if (!window.Promise) {
+      window.Promise = Promise;
+    }
 
     var options = this.options;
     var that = this;
@@ -32,7 +44,7 @@ function ContidioWidget(){
 
     fetch(url, {
       headers: {
-          'x-contidio-sdk': '1.0-JS'
+        'x-contidio-sdk': '1.0-JS'
       }
     }).then(function (response) {
       return response.json();
@@ -40,21 +52,21 @@ function ContidioWidget(){
 
       that.extractItems(json);
 
-      var renderer = new Renderer($, options);
+      var renderer = new Renderer(options, $);
 
       var $itemList = $("<div class='contidio-item-list'></div>");
 
-      for(var i = 0; i < that.items.length; i++) {
-          $itemList.append(renderer.renderListItem(that.items[i]));
+      for (var i = 0; i < that.items.length; i++) {
+        $itemList.append(renderer.renderListView(that.items[i]));
       }
 
       $(options.container).append($itemList);
 
-      if(renderer.resize){
+      if (renderer.resize) {
 
         renderer.resize();
-        that.addEvent(window, "resize", function(){
-            renderer.resize();
+        that.addEvent(window, "resize", function () {
+          renderer.resize();
         });
 
       }
@@ -62,7 +74,7 @@ function ContidioWidget(){
     });
   };
 
-  this.extractItems = function(json) {
+  this.extractItems = function (json) {
 
     var that = this;
 
@@ -75,7 +87,7 @@ function ContidioWidget(){
         previewImage: that.getPreviewImage(entity)
       };
 
-      if(entity.workingSetBinaryType){
+      if (entity.workingSetBinaryType) {
         item.binaryType = that.defineBinaryType(entity.workingSetBinaryType);
       }
 
@@ -83,11 +95,11 @@ function ContidioWidget(){
     });
   };
 
-  this.defineType = function(type) {
+  this.defineType = function (type) {
 
     /* TODO: switch to constants */
 
-    switch(type) {
+    switch (type) {
       case 1:
         return "collection";
       case 2:
@@ -98,62 +110,62 @@ function ContidioWidget(){
 
   };
 
-  this.defineBinaryType = function(binaryType) {
+  this.defineBinaryType = function (binaryType) {
 
     /* TODO: switch to constants */
 
-    switch(binaryType) {
+    switch (binaryType) {
       case 1:
-          return "image";
+        return "image";
       case 2:
-          return "audio";
+        return "audio";
       case 3:
-          return "video";
+        return "video";
       case 4:
-          return "document";
+        return "document";
       default:
-          return "undefined";
+        return "undefined";
     }
 
   };
 
-  this.getPreviewImage = function(entity) {
-      var indexToUse = -1;
+  this.getPreviewImage = function (entity) {
+    var indexToUse = -1;
 
-      if(entity.workingSetBinaryType == 2) {
+    if (entity.workingSetBinaryType == 2) {
 
-          if(entity.previewBinarySet[0].calculatedBinary.length > 1) {
+      if (entity.previewBinarySet[0].calculatedBinary.length > 1) {
 
-              for(var i = 0; i < entity.previewBinarySet[0].calculatedBinary.length; i++) {
-                  if(entity.previewBinarySet[0].calculatedBinary[i].binaryPurpose == 19000 &&
-                      entity.previewBinarySet[0].calculatedBinary[i].width >= 560
-                  ){
-                      indexToUse = i;
-                  }
-              }
+        for (var i = 0; i < entity.previewBinarySet[0].calculatedBinary.length; i++) {
+          if (entity.previewBinarySet[0].calculatedBinary[i].binaryPurpose == 19000 &&
+            entity.previewBinarySet[0].calculatedBinary[i].width >= 560
+          ) {
+            indexToUse = i;
           }
-
-      } else {
-          indexToUse = 0;
+        }
       }
 
-      if(indexToUse > -1) {
-        return entity.previewBinarySet[0].calculatedBinary[indexToUse].downloadLink;
-      } else {
-        return false;
-      }
+    } else {
+      indexToUse = 0;
+    }
+
+    if (indexToUse > -1) {
+      return entity.previewBinarySet[0].calculatedBinary[indexToUse].downloadLink;
+    } else {
+      return false;
+    }
 
   };
 
-  this.addEvent = function(object, type, callback) {
-      if (object == null || typeof(object) == 'undefined') return;
-      if (object.addEventListener) {
-          object.addEventListener(type, callback, false);
-      } else if (object.attachEvent) {
-          object.attachEvent("on" + type, callback);
-      } else {
-          object["on"+type] = callback;
-      }
+  this.addEvent = function (object, type, callback) {
+    if (object == null || typeof(object) == 'undefined') return;
+    if (object.addEventListener) {
+      object.addEventListener(type, callback, false);
+    } else if (object.attachEvent) {
+      object.attachEvent("on" + type, callback);
+    } else {
+      object["on" + type] = callback;
+    }
   };
 
 }
